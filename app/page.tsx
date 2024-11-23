4dcaaf6ce231cbb8e12bba3a72881d4016cff936
Marcus Vinicius <marcusv892@gmail.com>
2024-11-23 18:53:47 -0300

    chore: adjust clerk authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import Image from "next/image";
-import { Button } from "./_components/ui/button";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { UserButton } from "@clerk/nextjs";
-import { dark } from "@clerk/themes";
 
 export default async function HomePage() {
   const { userId } = await auth();
