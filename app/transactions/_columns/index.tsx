1aa4636c6ad7cf7083e54e870528c615accb512a
Marcus Vinicius <marcusv892@gmail.com>
2024-12-03 22:38:30 -0300

    feat: created badge component of the type table attribute

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import { Transaction } from "@prisma/client";
+import { Transaction, TransactionType } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
-
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
+import { Badge } from "@/app/_components/ui/badge";
+import { CircleIcon } from "lucide-react";
 
 export const transactionsColumns: ColumnDef<Transaction>[] = [
   {
@@ -14,6 +13,17 @@ export const transactionsColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "type",
     header: "Tipo",
+   cell: ({row: {original: transaction}}) => {
+    if(transaction.type == TransactionType.DEPOSIT ){
+     return <Badge variant={"outline"} className="bg-muted text-lime-500 bg-lime-500 bg-opacity-10 border-none font-bold"><CircleIcon className="fill-lime-500 mr-1" size={13}/>Ganho</Badge>
+    }
+    if(transaction.type == TransactionType.EXPENSE){
+      return <Badge variant={'outline'} className="bg-muted text-danger bg-danger bg-opacity-10 border-none font-bold"><CircleIcon className="fill-danger mr-1" size={13}/>Gasto</Badge>
+    }else{
+      return <Badge variant={'outline'} className="bg-muted text-white bg-white bg-opacity-10 border-none font-bold"><CircleIcon className="fill-white mr-1" size={13} />Investimento</Badge>
+    }
+   }
+    
   },
   {
     accessorKey: "category",
