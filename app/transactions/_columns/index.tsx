6b987024c9734e1eb9547d888c22f76c3ff5fc39
Marcus Vinicius <marcusv892@gmail.com>
2024-12-04 17:58:22 -0300

    feat: include rende component badge in data table

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -1,57 +1,53 @@
-"use client";
+"use client"
 
-import { Transaction, TransactionType } from "@prisma/client";
-import { ColumnDef } from "@tanstack/react-table";
-import { Badge } from "@/app/_components/ui/badge";
-import { CircleIcon } from "lucide-react";
+import { Transaction, TransactionCategory } from "@prisma/client"
+import { ColumnDef } from "@tanstack/react-table"
+import TransactionTypeBadge from "../_components/badges"
 
 export const transactionsColumns: ColumnDef<Transaction>[] = [
-  {
-    accessorKey: "name",
-    header: "Nome",
-  },
-  {
-    accessorKey: "type",
-    header: "Tipo",
-   cell: ({row: {original: transaction}}) => {
-    if(transaction.type == TransactionType.DEPOSIT ){
-     return <Badge variant={"outline"} className="bg-muted text-lime-500 bg-lime-500 bg-opacity-10 border-none font-bold"><CircleIcon className="fill-lime-500 mr-1" size={13}/>Ganho</Badge>
-    }
-    if(transaction.type == TransactionType.EXPENSE){
-      return <Badge variant={'outline'} className="bg-muted text-danger bg-danger bg-opacity-10 border-none font-bold"><CircleIcon className="fill-danger mr-1" size={13}/>Gasto</Badge>
-    }else{
-      return <Badge variant={'outline'} className="bg-muted text-white bg-white bg-opacity-10 border-none font-bold"><CircleIcon className="fill-white mr-1" size={13} />Investimento</Badge>
-    }
-   }
-    
-  },
-  {
-    accessorKey: "category",
-    header: "Categoria",
-  },
-  {
-    accessorKey: "paymentMethod",
-    header: "Método",
-  },
-  {
-    accessorKey: "date",
-    header: "Data",
-  },
-  {
-    accessorKey: "amount",
-    header: "Valor",
-    cell: ({ row }) => {
-      const amount = parseFloat(row.getValue("amount"));
-      const value = new Intl.NumberFormat("pt-br", {
-        style: "currency",
-        currency: "BRL",
-      }).format(amount);
+	{
+		accessorKey: "name",
+		header: "Nome",
+	},
+	{
+		accessorKey: "type",
+		header: "Tipo",
+		cell: ({ row: { original: transaction } }) => (
+			<TransactionTypeBadge transaction={transaction} />
+		),
+	},
+	{
+		accessorKey: "category",
+		header: "Categoria",
+		cell: ({ row: { original: transaction } }) => {
+			if (transaction.category == TransactionCategory.SERVICES) {
+				return "Serviços"
+			}
+		},
+	},
+	{
+		accessorKey: "paymentMethod",
+		header: "Método",
+	},
+	{
+		accessorKey: "date",
+		header: "Data",
+	},
+	{
+		accessorKey: "amount",
+		header: "Valor",
+		cell: ({ row }) => {
+			const amount = parseFloat(row.getValue("amount"))
+			const value = new Intl.NumberFormat("pt-br", {
+				style: "currency",
+				currency: "BRL",
+			}).format(amount)
 
-      return <div>{value}</div>;
-    },
-  },
-  {
-    accessorKey: "actions",
-    header: "",
-  },
-];
+			return <div>{value}</div>
+		},
+	},
+	{
+		accessorKey: "actions",
+		header: "",
+	},
+]
