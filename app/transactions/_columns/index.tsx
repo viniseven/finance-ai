9e9b45f36d731ed72dbd39ff86cc479485de129e
Marcus Vinicius <marcusv892@gmail.com>
2024-12-05 20:59:09 -0300

    feat: consts Transactions Category e Melhor Labels

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -1,8 +1,31 @@
 "use client"
 
-import { Transaction, TransactionCategory } from "@prisma/client"
+import { Transaction } from "@prisma/client"
 import { ColumnDef } from "@tanstack/react-table"
 import TransactionTypeBadge from "../_components/badges"
+
+export const TransactionCategoryLabel = {
+	SERVICES: "Serviços",
+	HOUSING: "Moradia",
+	TRANSPORTATION: "Transporte",
+	FOOD: "Comida",
+	ENTERTAINMENT: "Entretenimento",
+	HEALTH: "Saúde",
+	UTILITY: "Utilidade",
+	SALARY: "Ganhos",
+	EDUCATION: "Educação",
+	OTHER: "Outros",
+}
+
+export const TransactionMethodLabel = {
+	CREDIT_CARD: "Crédito",
+	DEBIT_CARD: "Débito",
+	BANK_TRANSFER: "Transferência",
+	BANK_SLIP: "Boleto",
+	CASH: "Dinheiro",
+	PIX: "Pix",
+	OTHER: "Outros",
+}
 
 export const transactionsColumns: ColumnDef<Transaction>[] = [
 	{
@@ -19,19 +42,24 @@ export const transactionsColumns: ColumnDef<Transaction>[] = [
 	{
 		accessorKey: "category",
 		header: "Categoria",
-		cell: ({ row: { original: transaction } }) => {
-			if (transaction.category == TransactionCategory.SERVICES) {
-				return "Serviços"
-			}
-		},
+		cell: ({ row: { original: transaction } }) =>
+			TransactionCategoryLabel[transaction.category],
 	},
 	{
 		accessorKey: "paymentMethod",
 		header: "Método",
+		cell: ({ row: { original: transaction } }) =>
+			TransactionMethodLabel[transaction.paymentMethod],
 	},
 	{
 		accessorKey: "date",
 		header: "Data",
+		cell: ({ row: { original: transaction } }) =>
+			new Date(transaction.date).toLocaleDateString("pt-BR", {
+				day: "2-digit",
+				month: "long",
+				year: "numeric",
+			}),
 	},
 	{
 		accessorKey: "amount",
